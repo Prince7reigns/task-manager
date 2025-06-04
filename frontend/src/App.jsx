a16e3747f43d6b68fec2d8244c943e8d48d7058d
Prince Yadav <prinsyadac@gmail.com>
2025-06-04 23:36:02 +0530

    feat: enhance App component with user authentication and routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,64 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import Navbar from './components/Navbar'
+import { Outlet,Route,Routes, useNavigate } from 'react-router-dom'
+import Layout from './components/Layout'
+import Login from './components/Login'
+import SignUp from './components/SignUp'
 
 function App() {
 
+  const navigate = useNavigate()
+
+  const [currentUser,setCurrentUser] = useState(()=>{
+    const storedUser = localStorage.getItem('currentUser')
+    return storedUser ? JSON.parse(storedUser) : null
+  })
+
+  useEffect(()=>{
+    if(currentUser){
+      localStorage.setItem('currentUser',JSON.stringify(currentUser))
+    }
+    else{
+      localStorage.removeItem('currentUser')
+    }
+  },[currentUser])
+
+  const handleAuthSubmit = data =>{
+    const user = {
+      email:data.email,
+      name:data.name || 'User',
+       avatar: `https://ui-avatars.com/api/?name=${encodeURIComponent(data.name || 'User')}&background=random`
+    }
+
+    setCurrentUser(user)
+    navigate('/',{replace:true})
+  }
+
+  const handelLogout = () =>{
+    localStorage.removeItem('token')
+    setCurrentUser(null)
+    navigate('/login', {replace:true})
+  }
+
+  const ProtectedLayout = ()=>{
+    <Layout user={currentUser} onLogout={handelLogout}>
+      <Outlet/>
+    </Layout>
+  }
+
   return (
-    <div>
-      LETS WATCH THE VIDEO AND CREATE THIS EXCITING TASKMANAGER WEB APP
-    </div>
+    <Routes>
+
+      <Route path='/login' element={<div className='fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center'>
+       <Login onSubmit={handleAuthSubmit} onSwitchMode={()=>navigate('/signup')}/>
+       </div>}/>
+
+       <Route path='/signup' element={<div className='fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center'>
+       <SignUp onSubmit={handleAuthSubmit} onSwitchMode={()=>navigate('/login')}/>
+       </div>}/>
+
+      <Route path='/' element={<Layout/>}/>
+    </Routes>
   )
 }
 
