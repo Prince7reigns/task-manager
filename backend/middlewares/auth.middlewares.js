c8ebbbaf0af68a099958a96208a71d18fcfe65bc
Prince Yadav <prinsyadac@gmail.com>
2025-06-07 00:29:25 +0530

    fix: remove unnecessary line break and improve token logging in verifyJWT middleware

--- backend/middlewares/auth.middlewares.js
+++ backend/middlewares/auth.middlewares.js
@@ -13,7 +13,9 @@ export const  verifyJWT = asyncHandler(async(req, _, next) =>{
          if (!token) {
             throw new ApiError(401, "Unauthorized  request")
         }
-
+ 
+        console.log("token", token);
+        
         const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
 
         const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
